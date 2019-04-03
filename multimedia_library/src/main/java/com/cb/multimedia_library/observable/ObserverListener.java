240c642ada20f8e8831fd0b4ae61515e3c188ed5
Chang Bo <you@example.com>
2019-04-03 11:38:14 +0800

    初次提交

---
+++ multimedia_library/src/main/java/com/cb/multimedia_library/observable/ObserverListener.java
@@ -0,0 +1,19 @@
+package com.cb.multimedia_library.observable;
+
+import com.cb.multimedia_library.entity.LocalMedia;
+import com.cb.multimedia_library.entity.LocalMediaFolder;
+
+import java.util.List;
+
+/**
+ * author：luck
+ * project：PictureSelector
+ * package：com.luck.picture.lib.observable
+ * email：893855882@qq.com
+ * data：17/1/16
+ */
+public interface ObserverListener {
+    void observerUpFoldersData(List<LocalMediaFolder> folders);
+
+    void observerUpSelectsData(List<LocalMedia> selectMedias);
+}
